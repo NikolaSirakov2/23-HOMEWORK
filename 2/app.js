b6e26481d909d550665c06dc2ec908b6b7ae776d
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2023-03-11 21:43:47 +0200

    Update app.js

--- 2/app.js
+++ 2/app.js
@@ -1,16 +1,42 @@
-let container = document.getElementById('container');
-let image = document.createElement("img");
+let randomFiveFoxImages = document.getElementById("foxImages");
 
-fetch('https://randomfox.ca/floof')
-        .then(response => {
-            if(response.ok){
-                return response.json();
-            } else {
-                console.log("Error!!!");
-            }
-        })
-        .then(body => {
-            console.log(body.link);
-            
-            
-        })
+randomFiveFoxImages.addEventListener("click", () => {
+
+  let container = document.getElementById("container");
+  container.innerHTML = "";
+
+  function easyFetch(url) {
+    return fetch(url).then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        return Promise.reject("Error!!!");
+      }
+    });
+  }
+
+  let foxes = Promise.all([
+    easyFetch("https://randomfox.ca/floof"),
+    easyFetch("https://randomfox.ca/floof"),
+    easyFetch("https://randomfox.ca/floof"),
+    easyFetch("https://randomfox.ca/floof"),
+    easyFetch("https://randomfox.ca/floof"),
+  ]).then((responses) => {
+    console.log(responses);
+    return responses;
+  });
+
+  foxes
+    .then((responses) => {
+      responses.forEach((elem) => {
+        let image = document.createElement("img");
+        image.src = elem.image;
+        image.style.width = "250px";
+
+        container.appendChild(image);
+      });
+    })
+    .catch(() => {
+      return console.log("We have a problem!!!");
+    });
+});
