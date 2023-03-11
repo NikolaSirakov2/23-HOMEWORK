f93b5e6c675a55a4f7555b47b540e30b1cad0f05
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2023-03-11 16:36:06 +0200

    Update app.js

--- 2/app.js
+++ 2/app.js
@@ -1,3 +1,10 @@
+let container = document.getElementById('container');
 
-
-
+fetch('https://randomfox.ca/floof')
+        .then(response => {
+            if(response.ok){
+                return response.json();
+            } else {
+                console.log("Error!!!");
+            }
+        })
