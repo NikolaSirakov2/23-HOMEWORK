bab45e94c1f280f5731d8735ce51b9f46b72fca5
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2023-03-12 12:16:08 +0200

    Update app.js

--- 4/app.js
+++ 4/app.js
@@ -28,11 +28,20 @@
       
         for(let n = 0; n < response[0].length; n++){
             console.log(response[0][n]);
-        let email = document.createElement("option");
-        email.value = response[0][n].email;
-        email.innerText = response[0][n].email;
+            let email = document.createElement("option");
+            email.value = response[0][n].email;
+            email.innerText = response[0][n].email;
+            emails.appendChild(email);
 
-        emails.appendChild(email);
+            let selectedId = response[0][n].id;
+
+            let postsReq = easyFetch(`https://jsonplaceholder.typicode.com/users/${selectedId}/posts`)
+                    .then((posts) => {
+                        console.log(posts);
+                        return posts;
+                    });
+
+
         }
     })
     .catch(() => {
