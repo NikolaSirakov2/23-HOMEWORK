caf25ac542aea601fd8737c0367fc1f92c45a267
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2023-03-13 15:55:49 +0200

    Update app.js

--- 8/app.js
+++ 8/app.js
@@ -1,6 +1,6 @@
 let table = document.getElementById("table");
 
-for(let i = 0; i < 9; i++){
+for(let i = 0; i < 10; i++){
     let request = fetch(`https://api.thecatapi.com/v1/images/search?`)
     .then((response) => {
         if (response.ok) {
@@ -10,31 +10,25 @@ for(let i = 0; i < 9; i++){
         }
       })
       .then((body) => {
-        return body;
-      });
-
-    request
-        .then((responses) => {
-            console.log(responses);
-            let newRow = document.createElement('tr')
+        console.log(body[0].url);
+        let newRow = document.createElement('tr')
                
-            let id = document.createElement("td");
-            let url = document.createElement('td');
-            let width = document.createElement('td');
-            let height = document.createElement('td');
-
-            id.innerText = responses[i].id;
-            url.innerText = responses[i].url;
-            width.innerText = responses[i].width;
-            height.innerText = responses[i].height;
-
-            newRow.append(id, url, width, height);
-
-            table.appendChild(newRow);
-        
-        })
-        .catch(() => {
-          return console.log("We have a problem!!!");
-        });
+                let id = document.createElement("td");
+                let url = document.createElement('td');
+                let width = document.createElement('td');
+                let height = document.createElement('td');
     
-}
+                id.innerText = body[0].id;
+                url.innerText = body[0].url;
+                width.innerText = body[0].width;
+                height.innerText = body[0].height;
+    
+                newRow.append(id, url, width, height);
+    
+                table.appendChild(newRow);
+            
+            })
+            .catch(() => {
+              return console.log("We have a problem!!!");
+            });
+      }
