ca217a978a2f4bc7733480288ef3af2a480474f0
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2023-03-13 19:49:25 +0200

    Update app.js

--- 9/app.js
+++ 9/app.js
@@ -1,11 +1,20 @@
 let requestButton = document.getElementById("requestButton");
+let imageContainer = document.getElementById("container");
+let loading = document.getElementById("Loading");
 
-requestButton.addEventListener('click', (e) => {
-    let request = fetch("https://randomfox.ca/floof/")
-            .then((response) => {
-                return response.json();
-            })
-            .then(response => {
-                console.log(response.image);
-            })
-})
+requestButton.addEventListener("click", (e) => {
+  loading.innerText = "Loading...";
+  imageContainer.innerHTML = "";
+  let request = fetch("https://randomfox.ca/floof/")
+    .then((response) => {
+      return response.json();
+    })
+    .then((response) => {
+      let image = document.createElement("img");
+      image.src = response.image;
+      image.style.width = "500px";
+
+      imageContainer.appendChild(image);
+      loading.innerText = "";
+    });
+});
