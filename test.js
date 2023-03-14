fa821afa507ddc574a61a6b66083680c864975cc
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2023-03-14 18:30:11 +0200

    Delete test.js

--- test.js
+++
@@ -1,9 +0,0 @@
-function easyFetch(url) {
-    return fetch(url).then((response) => {
-      if (response.ok) {
-        return response.json();
-      } else {
-        return Promise.reject("Error!!!");
-      }
-    });
-  }
